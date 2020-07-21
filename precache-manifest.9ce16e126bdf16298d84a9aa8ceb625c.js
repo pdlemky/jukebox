de331a8a0931b51c25a549726f4a065b5c2792b6
Paul Lemky <paul@Pauls-MacBook-Pro.local>
2020-07-20 23:14:48 -0700

    Updates

---
+++ precache-manifest.9ce16e126bdf16298d84a9aa8ceb625c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3c07cf3bfa65d67b23ee61f4b52c9e",
+    "url": "/pdlemky/jukebox/index.html"
+  },
+  {
+    "revision": "489dd21058d25e1e335b",
+    "url": "/pdlemky/jukebox/static/css/2.c76073f2.chunk.css"
+  },
+  {
+    "revision": "5340e2f924c81de418da",
+    "url": "/pdlemky/jukebox/static/css/main.1ad74c9d.chunk.css"
+  },
+  {
+    "revision": "489dd21058d25e1e335b",
+    "url": "/pdlemky/jukebox/static/js/2.26897ee0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pdlemky/jukebox/static/js/2.26897ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5340e2f924c81de418da",
+    "url": "/pdlemky/jukebox/static/js/main.8a98b953.chunk.js"
+  },
+  {
+    "revision": "9344b4bbadeede50ca23",
+    "url": "/pdlemky/jukebox/static/js/runtime-main.a51a3fc4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pdlemky/jukebox/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pdlemky/jukebox/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pdlemky/jukebox/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pdlemky/jukebox/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pdlemky/jukebox/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
